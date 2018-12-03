f5727bd418286053fabf957eb46862ecda9031d5
Chris Rabl <rabl.christopher@gmail.com>
2018-12-02 22:17:00 -0700

    omg seriously i could have got a tshirt

--- app.ts
+++ app.ts
@@ -2,22 +2,94 @@ const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-
+const uuid = require('uuid');
 
 const app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+enum TeamPosition {
+  Lead = 'LEAD',
+  Second = '2ND', 
+  Third = '3RD', 
+  Skip = 'SKIP'
+}
+
+const positions = [
+  TeamPosition.Lead,
+  TeamPosition.Second,
+  TeamPosition.Third, 
+  TeamPosition.Skip
+];
+
+interface Shot {
+  position: TeamPosition;
+  rock: number;
+  rating: number;
+}
+
+interface GameState {
+  end: number;
+  position: TeamPosition;
+  rock: number;
+  shots: Shot[];
+}
+
+function nextState(current_state: GameState): GameState {
+  let next_state = {
+    end: current_state.end,
+    position: current_state.position,
+    rock: current_state.rock,
+    shots: current_state.shots
+  };
+
+  if (current_state.rock === 2) {
+    // if it's the skip's second rock, increment end number
+    if (current_state.position === TeamPosition.Skip) {
+      next_state.end = current_state.end + 1;
+    }
+
+    // rotaté
+    const pos_index = positions.indexOf(current_state.position);
+    next_state.position = positions[(pos_index + 1) % 4]; 
+  }
+
+  // calculate next rock number
+  next_state.rock = current_state.rock % 2 + 1;
+
+  return next_state;
+}
+
+const states = {
+  '1ef7f9f4-80ec-4e7d-a4bc-549a22873157': {
+    end: 1,
+    position: TeamPosition.Lead,
+    rock: 1
+  }
+}
 
 app.get('/', (req, res) => {
-  const state = {
-    end: 1,
-    position: '2ND',
-    rock: 2
-  };
+  res.send('curlbot 1.0');
+})
+
+app.get('/:game_id', (req, res) => {
+  const { game_id } = req.params;
+  const state = states[game_id];
+  console.log(state);
   
-  return res.render('index', state);
+  return res.render('game', {
+    ...state,
+    game_id
+  });
+});
+
+app.post('/:game_id', (req, res) => {
+  const { game_id } = req.params;
+  console.log(req.body);
+  return res.send('OK')
 });
 
 const PORT = process.env.PORT || 8080;
